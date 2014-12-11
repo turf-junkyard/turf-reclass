8a5c6e6c85da858dd7c30c79fa8c55ff7d3db143
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 16:43:54 -0500

    styles

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var featurecollection = require('turf-featurecollection')
-var reclass = require('./index.js')
+var featurecollection = require('turf-featurecollection');
+var reclass = require('./index.js');
 
 module.exports = function(fc, inField, outField, translations, done){
-  var reclassed = featurecollection([])
+  var reclassed = featurecollection([]);
 
   fc.features.forEach(function(feature){
-    var reclassedFeature
-    var found = false
+    var reclassedFeature;
+    var found = false;
     for(var i = 0; i < translations.length; i++){
       if(feature.properties[inField] >= translations[i][0] && feature.properties[inField] <= translations[i][1]) {
-        feature.properties[outField] = translations[i][2]
+        feature.properties[outField] = translations[i][2];
       }
     }
-    reclassed.features.push(feature)
+    reclassed.features.push(feature);
   })
 
   return reclassed;
